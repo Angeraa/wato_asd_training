90d441142d3977b80fd6c8d2b7983296f15686ef
Angeraa <andrew.nicholas.gera@gmail.com>
2025-09-14 15:42:10 -0400

    Costmap and memory node basic

--- src/robot/costmap/include/costmap_node.hpp
+++ src/robot/costmap/include/costmap_node.hpp
@@ -1,16 +1,42 @@
 #ifndef COSTMAP_NODE_HPP_
 #define COSTMAP_NODE_HPP_
+ 
+#include <vector>
 
 #include "rclcpp/rclcpp.hpp"
-
+#include "sensor_msgs/msg/laser_scan.hpp"
+#include "nav_msgs/msg/occupancy_grid.hpp"
+#include "nav_msgs/msg/odometry.hpp"
+ 
 #include "costmap_core.hpp"
-
+ 
 class CostmapNode : public rclcpp::Node {
   public:
     CostmapNode();
-
+ 
   private:
-    robot::CostmapCore costmap_;
-};
+    void laserCallback(const sensor_msgs::msg::LaserScan::SharedPtr msg);
+    void odomCallback(const nav_msgs::msg::Odometry::SharedPtr msg);
 
-#endif 
+    void inflateObstacles(int radius);
+
+    void publishCostmap();
+
+
+    std::vector<std::vector<int>> costmap_data_;
+
+    double resolution_;
+    double size_m_;
+    int width_, height_;
+
+    robot::CostmapCore costmap_;
+
+    rclcpp::Subscription<sensor_msgs::msg::LaserScan>::SharedPtr laser_sub_;
+    rclcpp::Publisher<nav_msgs::msg::OccupancyGrid>::SharedPtr grid_pub_;
+    nav_msgs::msg::OccupancyGrid grid_msg_;
+    sensor_msgs::msg::LaserScan::SharedPtr last_scan_;
+
+    rclcpp::TimerBase::SharedPtr timer_;
+};
+ 
+#endif
